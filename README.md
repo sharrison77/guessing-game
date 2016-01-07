bf48ec0793d7e4d3a27db7ea9753c4b67e83513b
Zach Simonson <zsimonson26@gmail.com>
2016-01-07 12:54:18 -0800

    added modular functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # guessing-game
 This is a guessing game that uses Javascript.
+
+10/7: Added functions to code -Zach
--- app.js
+++ app.js
@@ -4,73 +4,92 @@ var userName = prompt('What is your name?');
 
 var counter = 0;
 
-var answer1 = prompt(userName + ', does Susan have children? Yes or No?').toUpperCase();
-  console.log ('The user answered Question 1: ' + answer1);
-
-var check1 = false;
-  do {
-    if (answer1 === 'YES' || answer1 === 'Y') {
-      alert('That\'s correct. She has 2 children.');
-      counter = counter + 1;
-      check1 = true;
-    } else if (answer1 === 'NO' || answer1 === 'N') {
-      alert('That\'s incorrect. She has children.');
-      check1 = true;
-    } else {
-      alert('Please answer with \'Yes\' or \'No\'.');
-      var answer1 = prompt(userName + ', does Susan have children? Yes or No?').toUpperCase();
-      console.log ('The user answered Question 1: ' + answer1);
-    }
-  } while (check1 === false);
-
-var answer2 = prompt(userName + ', does Susan have a cell phone? Yes or No').toUpperCase();
-console.log ('The user answered Question 2: ' + answer2);
-
-var check1 = false;
-  do {
-    if (answer2 === 'YES' || answer2 === 'Y') {
-      alert('That incorrect. She hasn\'t had a cell phone for the past 5 years.');
-      check1 = true;
-      } else if (answer2 === 'NO' || answer2 === 'N') {
-        alert('That\'s correct. She doesn\'t have a cell phone.');
-        counter = counter + 1
+function askQuestionOne(){
+  var answer1 = prompt(userName + ', does Susan have children? Yes or No?').toUpperCase();
+    console.log ('The user answered Question 1: ' + answer1);
+    
+  var check1 = false;
+    do {
+      if (answer1 === 'YES' || answer1 === 'Y') {
+        alert('That\'s correct. She has 2 children.');
+        counter = counter + 1;
+        check1 = true;
+      } else if (answer1 === 'NO' || answer1 === 'N') {
+        alert('That\'s incorrect. She has children.');
         check1 = true;
       } else {
         alert('Please answer with \'Yes\' or \'No\'.');
-        var answer2 = prompt(userName + ', does Susan have a cell phone? Yes or No').toUpperCase();
-        console.log ('The user answered Question 2: ' + answer2);
+        var answer1 = prompt(userName + ', does Susan have children? Yes or No?').toUpperCase();
+        console.log ('The user answered Question 1: ' + answer1);
       }
-} while (check1 === false);
+    } while (check1 === false);
+  };
 
-var answer3 = prompt(userName + ', does Susan live in Everett?').toUpperCase();
-console.log('The user answered Question 3: ' + answer3);
 
-var check1 = false
-  do {
-    if (answer3 === 'YES'|| answer3 === 'Y') {
-      alert('That\'s correct. She takes the bus to school.');
-      counter = counter + 1;
-      check1 = true;
-      } else if (answer3 === 'NO' || answer3 === 'N') {
-        alert('That\'s incorrect. She lives in Everett.');
+function askQuestionTwo() {
+  var answer2 = prompt(userName + ', does Susan have a cell phone? Yes or No').toUpperCase();
+  console.log ('The user answered Question 2: ' + answer2);
+
+  var check1 = false;
+    do {
+      if (answer2 === 'YES' || answer2 === 'Y') {
+        alert('That incorrect. She hasn\'t had a cell phone for the past 5 years.');
         check1 = true;
-      }else {
-        alert('Please answer with \'Yes\' or \'No\'.');
-      }
-} while (check1 === false);
+        } else if (answer2 === 'NO' || answer2 === 'N') {
+          alert('That\'s correct. She doesn\'t have a cell phone.');
+          counter = counter + 1
+          check1 = true;
+        } else {
+          alert('Please answer with \'Yes\' or \'No\'.');
+          var answer2 = prompt(userName + ', does Susan have a cell phone? Yes or No').toUpperCase();
+          console.log ('The user answered Question 2: ' + answer2);
+        }
+      } while (check1 === false);
+};
 
-var correct = false;
-while (correct === false){
-  var answer4 = prompt(userName + ', Guess a number between 1 and 10');
-  console.log('The user answered Questions 4: ' + answer4);
-  if (answer4 <= 4) {
-      alert('That number is too low.');
-    }else if (answer4 >=6)
-    {
-      alert('That number is too high.');
-    }else {
-      alert('That\'s the correct number.');
-        correct = true;
+
+function askQuestionThree() {
+  var answer3 = prompt(userName + ', does Susan live in Everett?').toUpperCase();
+  console.log('The user answered Question 3: ' + answer3);
+
+  var check1 = false
+    do {
+      if (answer3 === 'YES'|| answer3 === 'Y') {
+        alert('That\'s correct. She takes the bus to school.');
+        counter = counter + 1;
+        check1 = true;
+        } else if (answer3 === 'NO' || answer3 === 'N') {
+          alert('That\'s incorrect. She lives in Everett.');
+          check1 = true;
+        }else {
+          alert('Please answer with \'Yes\' or \'No\'.');
+        }
+      } while (check1 === false);
+};
+
+function askQuestionFour() {
+  var correct = false;
+  while (correct === false){
+    var answer4 = prompt(userName + ', Guess a number between 1 and 10');
+    answer4 = parseInt(answer4);
+    console.log('The user answered Questions 4: ' + answer4);
+    if (answer4 <= 4) {
+        alert('That number is too low.');
+      }else if (answer4 >=6)
+      {
+        alert('That number is too high.');
+      }else if (answer4 === 5){
+        alert('That\'s the correct number.');
+          correct = true;
+      }else{
+        alert('Please answer with a number')
       }
-    }
-    alert('You got ' + counter + ' correct.');
+      }
+  };
+
+askQuestionOne();
+askQuestionTwo();
+askQuestionThree();
+askQuestionFour();
+
+alert('You got ' + counter + ' correct.');
